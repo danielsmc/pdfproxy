5757c48a1b59b863d64df7c231a532b233cdd965
Daniel McLaughlin <danielsmc@gmail.com>
2017-01-16 08:27:09 -0500

    What a drag

--- source/main.js
+++ source/main.js
@@ -11,3 +11,34 @@ Array.prototype.forEach.call(document.querySelectorAll(".c-window__close"), func
     win.className = win.className.replace(/\bo-window--open\b/g,"");
   });
 });
+
+function makeDraggable(selector,grabber_selector) {
+  Array.prototype.forEach.call(document.querySelectorAll(selector), function(el) {
+    var graboffsetx, graboffsety, beingDragged=false;
+    var grabber_els = grabber_selector ? el.querySelectorAll(grabber_selector) : [el];
+    Array.prototype.forEach.call(grabber_els, function(grabber_el) {
+      grabber_el.addEventListener("mousedown", function(event) {
+        beingDragged = true;
+        var bounds = el.getBoundingClientRect();
+        var style = window.getComputedStyle(el);
+        graboffsetx = event.screenX - (bounds.left-parseFloat(style['margin-left']));
+        graboffsety = event.screenY - (bounds.top-parseFloat(style['margin-top']));
+      });
+      grabber_el.addEventListener("mouseup", function(event) {
+        beingDragged = false;
+      });
+      grabber_el.addEventListener("mousemove", function(event) {
+        beingDragged = beingDragged && ((event.buttons % 2) == 1);
+        if (beingDragged) {
+          el.style.position = "absolute";
+          el.style.left = "" + (event.screenX-graboffsetx) + "px";
+          el.style.top = "" + (event.screenY-graboffsety) + "px";
+        }
+      });
+    });
+  });
+}
+
+makeDraggable(".c-icon");
+makeDraggable(".o-window",".o-window__titlebar");
+
